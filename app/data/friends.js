cb075db313089a60635be60869c665b02325c1e4
erin hubbard <erinhubbard1981@gmail.com>
2018-03-20 21:29:39 -0400

    doesn't like /api/friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friends = [
   {
-    "name": "",
-    "photo": "",
-    "answer": []
+    "name": "John B",
+    "photo": "/images/John B.jpg",
+    "answers": [1, 3, 4, 1, 1, 2, 3, 4, 5, 5]
   },
   {
-    "name": "",
-    "photo": "",
-    "answer": []
+    "name": "Florence D",
+    "photo": "/images/Florence D.jpg",
+    "answers": [2, 3, 4, 1, 1, 5, 4, 1, 2, 2]
   },
   {
-    "name": "",
-    "photo": "",
-    "answer": []
+    "name": "Paula R",
+    "photo": "/images/Paula R.jpg",
+    "answers": [1, 1, 2, 3, 4, 5, 5, 4, 1, 2]
   }
 ];
 
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,15 +1,30 @@
 var friendData = require("../data/friends.js");
+var path = require("path");
 
 module.exports = function(app) {
 
  app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json(friendData);
   });
 
 app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toUpperCase();
-    console.log(newFriend);
+    var userScores = newFriend.answers
+    var matchName = "";
+    var matchPic = "";
+    var totalDifference = 100;
+
+    for (var i = 0; i < friendData.length; i++) {
+    	var diff = 0;
+    	for (var j = 0; j < userScores.length; j++) {
+    		diff += Math.abs(friendData[i].answers[j] - userScores[j]);
+    	} 
+    if (diff < totalDifference) {
+    	totalDifference = diff;
+    	matchName = friendData[i].name;
+    	matchPic = friendData[i].photo;
+    }
+   }
 
   friends.push(newFriend);
 
