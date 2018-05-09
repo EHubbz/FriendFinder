afe218d97f506fd8ffc97d84c184877a3654990d
erin hubbard <erinhubbard1981@gmail.com>
2018-05-09 16:55:57 -0400

    changed to index

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/survey", function(req, res) {
@@ -13,7 +13,7 @@ app.get("/survey", function(req, res) {
 });
 // Get all characters
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 };
 
